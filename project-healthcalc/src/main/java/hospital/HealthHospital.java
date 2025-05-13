b1a64840cd058ea13540d895cbd0f0a828c2abd0
Anabel Flores <anabelfl>
2025-05-13 18:34:08 +0200

    Fix: Adjust files

--- project-healthcalc/src/main/java/hospital/HealthHospital.java
+++
@@ -1,6 +0,0 @@
-package hospital;
-
-public interface HealthHospital {
-    float idealWeight(float height, char gender);
-    float basalMetabolicRate(float weight, float height, int age, char gender);
-}
